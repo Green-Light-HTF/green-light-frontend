1890c469291d76375f7ecca922026be39d83ed76
Zeel Pathak <zeelpathak2018.it@gmail.com>
2023-02-04 01:58:03 +0530

    formatted doc

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,31 +6,31 @@ export default function HomePage() {
     const [isModalOpen, setIsModalOpen] = useState(false);
 
     const showModal = () => {
-      setIsModalOpen(true);
+        setIsModalOpen(true);
     };
-  
+
     const handleOk = () => {
-      setIsModalOpen(false);
+        setIsModalOpen(false);
     };
-  
+
     const handleCancel = () => {
-      setIsModalOpen(false);
+        setIsModalOpen(false);
     };
-    
-  return (
-    <>
-    <div style={{display:"flex", alignItems: "center", justifyContent: "center", height:"100vh", width:"100vw"}}>
-        <div> 
-        <Button type="primary" onClick={showModal}>
-        Open Modal
-      </Button>
-      <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-      </Modal>
-        </div>
-    </div>
-    </>
-  );
+
+    return (
+        <>
+            <div style={{ display: "flex", alignItems: "center", justifyContent: "center", height: "100vh", width: "100vw" }}>
+                <div>
+                    <Button type="primary" onClick={showModal}>
+                        Open Modal
+                    </Button>
+                    <Modal title="SOS" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+                        <p>Some contents...</p>
+                        <p>Some contents...</p>
+                        <p>Some contents...</p>
+                    </Modal>
+                </div>
+            </div>
+        </>
+    );
 }
