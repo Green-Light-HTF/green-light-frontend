424fecbad5568c0a6cfc9d23eb9c5239b7fe31ff
Zeel Pathak <zeelpathak2018.it@gmail.com>
2023-02-04 02:24:00 +0530

    Added form in page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { Button, Modal } from 'antd';
+import { Button, Modal, Form, Input, InputNumber, message, Typography, Space } from 'antd';
 
 export default function HomePage() {
 
+    const [form] = Form.useForm();
+    const nameValue = Form.useWatch('name', form);
     const [isModalOpen, setIsModalOpen] = useState(false);
 
     const showModal = () => {
@@ -17,6 +19,15 @@ export default function HomePage() {
         setIsModalOpen(false);
     };
 
+    const onFinish = (values) => {
+        console.log(values)
+        message.success('Submit success!');
+    };
+
+    //   const onFinishFailed = () => {
+    //     message.error('Submit failed!');
+    //   };
+
     return (
         <>
             <div style={{ display: "flex", alignItems: "center", justifyContent: "center", height: "100vh", width: "100vw" }}>
@@ -24,10 +35,31 @@ export default function HomePage() {
                     <Button type="primary" onClick={showModal}>
                         Open Modal
                     </Button>
-                    <Modal title="SOS" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-                        <p>Some contents...</p>
-                        <p>Some contents...</p>
-                        <p>Some contents...</p>
+                    <Modal centered="true" title="SOS" open={isModalOpen} footer="" onCancel={handleCancel}>
+                        <Form form={form} layout="vertical" autoComplete="off" onFinish={onFinish} >
+
+                            <Form.Item name="emergency" label="Tell us about Emergency"
+                                rules={[
+
+                                    {
+                                        required: true,
+                                        message: 'Please, Tell us your Emergency',
+                                    },
+                                ]}>
+                                <Input />
+                            </Form.Item>
+                            <Form.Item name="information" label="Any Additional Information (Optional)">
+                                <Input />
+                            </Form.Item>
+                            <Form.Item>
+                                <Space>
+                                    <Button type="primary" htmlType="submit">
+                                        Submit
+                                    </Button>
+                                </Space>
+                            </Form.Item>
+
+                        </Form>
                     </Modal>
                 </div>
             </div>
