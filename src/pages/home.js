97827bbbf1a7c62bd2b01f7e78a9ffe4e09438ac
Zeel Pathak <zeelpathak2018.it@gmail.com>
2023-02-04 01:55:48 +0530

    Added modal and button

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,36 @@
 import React, { useState, useEffect } from "react";
-
+import { Button, Modal } from 'antd';
 
 export default function HomePage() {
 
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const showModal = () => {
+      setIsModalOpen(true);
+    };
+  
+    const handleOk = () => {
+      setIsModalOpen(false);
+    };
+  
+    const handleCancel = () => {
+      setIsModalOpen(false);
+    };
+    
   return (
     <>
-    HELLO WORLD
+    <div style={{display:"flex", alignItems: "center", justifyContent: "center", height:"100vh", width:"100vw"}}>
+        <div> 
+        <Button type="primary" onClick={showModal}>
+        Open Modal
+      </Button>
+      <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal>
+        </div>
+    </div>
     </>
   );
 }
